5b5944ad1841d1e418552b53d52584a266429636
Blake Stickland <blakelists@yahoo.com.au>
2022-01-30 23:28:02 +1100

    start of implementing JWT. Can login using JWT and API is set up to provide authentication plus a refresh token.

---
+++ todo-app-backend/restful-web-services/src/main/java/com/blakelists/rest/basic/auth/HelloWorldBean.java
@@ -0,0 +1,29 @@
+package com.blakelists.rest.basic.auth;
+
+public class HelloWorldBean {
+	
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+	
+	
+
+	public String getMessage() {
+		return message;
+	}
+
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HellowWorldBean [message=" + message + "]";
+	}
+	
+	
+}
