e42e3a79fbfb704966ddc7f4b93e011ba8c02586
Blake Stickland <blakelists@yahoo.com.au>
2022-02-05 23:08:12 +1100

    have created a sql seeder file to populate an H2 database with intiial todos

---
+++ todo-app-backend/restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO todo(id, username, description, target_date, is_done)
+values(10001, 'blakelists', 'Learn JPA', sysdate(), false);
+
+INSERT INTO todo(id, username, description, target_date, is_done)
+values(10002, 'blakelists', 'Learn to cook', sysdate(), false);
+
+INSERT INTO todo(id, username, description, target_date, is_done)
+values(10003, 'blakelists', 'Learn how to play guitar', sysdate(), false);
+
+INSERT INTO todo(id, username, description, target_date, is_done)
+values(10004, 'blakelists', 'Learn how to bang drums', sysdate(), false);
